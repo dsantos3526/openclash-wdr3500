d4a1d666230181837c6d1021af0d741f65382382
diyan santoso <diyan3526@gmail.com>
2021-08-27 13:36:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,28 @@
 #!/bin/ash
 echo "Download Luci-app-Clash"
 wget -O /tmp/luci-app-clash.ipk https://github.com/frainzy1477/luci-app-clash/releases/download/v1.7.5.7/luci-app-clash_v1.7.5.7_all.ipk
-echo "---------------------------------------------------------------------download Done----------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "Download Ipk Done"
 cd /tmp/
 opkg update && opkg install luci-app-clash.ipk
-wget -O /etc/clash/clash https://github.com/frainzy1477/clash_dev/releases/download/v1.1.0/clash-linux-mips-softfloat.gz 
+echo "Install luci app ipk done"
+
+wget -O /etc/clash/clash-linux-mips-softfloat.gz https://github.com/frainzy1477/clash_dev/releases/download/v1.1.0/clash-linux-mips-softfloat.gz 
 opkg update && opkg install gzip 
 cd /etc/clash/
 gunzip clash-linux-mips-softfloat.gz
 chmod +x clash-linux-mips-softfloat
 mv clash-linux-mips-softfloat /etc/clash/clash
 
-wget -O /etc/clash/clashtun https://github.com/comzyh/clash/releases/download/20210310/clash-linux-mips-softfloat-20210310.gz
+wget -O /etc/clash/clashtun/clash-linux-mips-softfloat-20210310.gz https://github.com/comzyh/clash/releases/download/20210310/clash-linux-mips-softfloat-20210310.gz
 cd /etc/clash/clashtun/
-gunzip clash-linux-mips-softfloat-20210310
+gunzip clash-linux-mips-softfloat-20210310.gz
 chmod +x clash-linux-mips-softfloat-20210310
 mv clash-linux-mips-softfloat-20210310 /etc/clash/clashtun/clash
 
-wget -O /etc/clash/dtun/clash https://github.com/Dreamacro/clash/releases/download/premium/clash-linux-mips-softfloat-2021.07.03.gz
+wget -O /etc/clash/dtun/clash-linux-mips-softfloat-20210310.gz https://github.com/Dreamacro/clash/releases/download/premium/clash-linux-mips-softfloat-2021.07.03.gz
 cd /etc/clash/dtun/
-gunzip clash-linux-mips-softfloat-20210310
+gunzip clash-linux-mips-softfloat-20210310.gz
 chmod +x clash-linux-mips-softfloat-20210310
 mv clash-linux-mips-softfloat-20210310 /etc/clash/dtun/clash
 
-echo"Done"
+echo "Done"
